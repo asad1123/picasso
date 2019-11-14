16db3348d434222d7aacf04e380702642c2e595d
Arjun Sadanand <asadanand3@gmail.com>
2019-11-13 23:31:50 -0500

    first cut of frontend

--- picasso/routes.py
+++
@@ -1,5 +0,0 @@
-from . import app, views
-
-# ----------------------------------------------------------------------------
-
-app.add_url_rule("/images/", view_func=views.ImageView.as_view("get_images"))
--- picasso/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# ----------------------------------------------------------------------------
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-    f"sqlite:///{os.path.join(basedir, 'app.db')}"
-
-IMG_SRC_URL = os.environ["IMG_SRC_URL"]
